04e54c2a3824c642cac46cbd7149a052a410dcc7
Roman Perekrestov <mr.perekrestov@gmail.com>
2019-10-28 15:03:57 +0100

    changes

--- PrimeNumbers.ConsoleApp/Program.cs
+++ PrimeNumbers.ConsoleApp/Program.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace PrimeNumbers.ConsoleApp
 {
@@ -9,25 +11,42 @@ namespace PrimeNumbers.ConsoleApp
     {
         static void Main(string[] args)
         {            
-            if (File.Exists("Result.txt"))
+            if (File.Exists("Result.csv"))
             {
-                File.Delete("Result.txt");
+                File.Delete("Result.csv");
             }
+            var acquisitionTimeSeconds = 60;
+            var logginPeriodMilliseconds = 200;
+            var timeStarted = DateTime.Now;
             var resultList = new List<PrimeNumbersProgressModel>();
-            void reportAction(PrimeNumbersProgressModel progressModel)
-            {               
-                resultList.Add(progressModel);               
-            }
             var finder = new PrimeNumbersFinder(reportAction);
+            Console.CursorVisible = false;
+            var timer = new Timer((obj) =>
+            {               
+                var timePassed = DateTime.Now.Subtract(timeStarted);
+                Console.Write($" Seconds passed: {Convert.ToInt32(timePassed.TotalSeconds)}");
+                Console.CursorLeft = 0;
+            },null,0,1000);
+
+            finder.FindPrimes(
+                new TimeSpan(0,0, acquisitionTimeSeconds),
+                new TimeSpan(0, 0, 0, 0, logginPeriodMilliseconds));
             
-            finder.FindPrimes(new TimeSpan(0, 0, 10), new TimeSpan(0, 0, 0, 0, 200));
             foreach(var progressItem in resultList)
             {
-                var resultString = $"passed: {Convert.ToInt32(progressItem.MillisecondsPassed.TotalMilliseconds)} ms," +
-                    $" number of primes: {progressItem.NumberOfPrimeNumbers}," +
-                    $" number of iterations {progressItem.NumberOfIterations}";
-                File.AppendAllText("Result.txt", resultString + "\n");
+                var resultString = $"{Convert.ToInt32(progressItem.MillisecondsPassed.TotalMilliseconds)}," +
+                    $"{progressItem.NumberOfPrimeNumbers}, " +
+                    $"{progressItem.NumberOfIterations}, " +
+                    $"{progressItem.MaxPrimeNumber}";
+                File.AppendAllText("Result.csv", resultString + "\n");
             }
+
+            void reportAction(PrimeNumbersProgressModel progressModel)
+            {
+                resultList.Add(progressModel);
+            }
+            Console.CursorVisible = true;
+            timer?.Dispose();
         }
     }
 }
